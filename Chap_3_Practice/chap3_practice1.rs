2ac9a3766763542d31a4746246f7a00f1b2306cf
Sam Lijo <samuelvillalvalijo@gmail.com>
2022-01-26 17:13:10 -0600

    Messing around with input

--- Chap_3_Practice/chap3_practice1.rs
+++ Chap_3_Practice/chap3_practice1.rs
@@ -6,18 +6,40 @@
 //1/26/22
 
 use std::any::type_name;
+use std::io;
+
+//use rand::Rng;
+//can't use Rand because it is a create that needs to be added to the 
+// .toml file
 
 fn main(){
     let x: i32 = 78;
 
     let y: f32 = 12.89156;
 
+    //let z = rand::thread_rng().gen_range(1..10_000);
+
     println!("The value of x is: {}", x);
     println!("The data type of x is: {}", type_of(x));
     println!("The value of y is: {}", y);
     println!("The data type of y is: {}", type_of(y));
+
+    //println!("The value of the random number z is: {}", z);
+
+    println!("Input a floating point number: ");
+
+    let mut user_input_float = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input_float)
+        .expect("Failed to read line");
+
+    println!("You entered: {}", user_input_float);
+    println!("Type of what you entered: {}", type_of(user_input_float));
 }
 
 fn type_of<T>(_: T) ->&'static str {
     type_name::<T>()
+    //:: <T> is an associated function of type_name that acts on
+    //Type_name
 }
